026f12395fb147193e0707789026dc446af74cf9
tong <tong@disktree.net>
2014-04-21 11:39:28 +0200

    removed nodejs target from example

--- examples/browser/App.hx
+++ examples/browser/App.hx
@@ -1,22 +1,14 @@
 
-#if nodejs
-import js.Node;
-#else
 import js.Browser.document;
 import js.Browser.window;
-#end
 import otr.Session;
 
 class App {
 
 	static function append( msg : String ) {
-		#if nodejs
-		Node.console.log( msg );
-		#else
 		var p = document.createDivElement();
 		p.innerHTML = msg;
 		document.body.appendChild( p );
-		#end
 	}
 
 	static inline function now() : Float return Date.now().getTime();
@@ -48,17 +40,13 @@ class App {
 		userA.onStatus = function(state){
 			if( state == untyped OTR.CONST.STATUS_AKE_SUCCESS ) {
 				append( 'ake took <strong>' + (now() - start) + 'ms</strong>');
-				append( 'message state is ' + (userA.crypto.msgstate ? 'encrypted' : 'plaintext') + '</strong>');
+				append( 'message state is ' + (userA.crypto.msgstate ? 'encrypted' : 'plaintext') );
 			}
 		}
 	}
 
 	static function main() {
 		trace( "hxotr" );
-		#if nodejs
-		init();
-		#elseif js
 		window.onload = function(_) { init(); }
-		#end
 	}
 }
